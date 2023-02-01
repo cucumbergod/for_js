1b36edf68642460c93b21982bd8d4fd8d0ab1260
suser <122861016+cucumbergod@users.noreply.github.com>
2023-02-01 19:19:06 +0300

    Delete numb3.js

--- numb3.js
+++
@@ -1,7 +0,0 @@
-"use strict"
-let n = 10
-let m = -5
-let random =( n > m? Math.random(n,m) * (n - m) + m: Math.random(m,n) * (m - n) + n).toFixed(0)
-random % 2 == 0? random = random - 1: random = random 
-console.log(random)
-//не знаю, как сделать это без ветвления, особенно при разных n и m(в плане больше или меньше)
