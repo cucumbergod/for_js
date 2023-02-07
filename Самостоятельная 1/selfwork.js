8a06427b4190dda75927c26d965bf9e68e76038f
suser <122861016+cucumbergod@users.noreply.github.com>
2023-02-07 08:56:46 +0300

    Delete Самостоятельная 1 directory

--- Самостоятельная 1/selfwork.js
+++
@@ -1,4 +0,0 @@
-"use strict" 
-let password = "vknfdhdh"
-let passLength = password.length
-passLength > 4 || password.includes("_","-") ? console.log("Пароль надежный"):console.log("Пароль ненадежный")
